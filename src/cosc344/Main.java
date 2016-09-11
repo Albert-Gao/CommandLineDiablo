ed29c58534816b7f110f718ad7bf55099b6981fa
Albert Gao <albertgaohy@gmail.com>
2016-09-11 21:18:15 +1200

    added the entry point of the class

---
+++ src/cosc344/Main.java
@@ -0,0 +1,38 @@
+package cosc344;
+
+import cosc344.utils.ConnectionManager;
+import cosc344.utils.GameManager;
+import cosc344.utils.SceneGenerator;
+
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            Scanner scanner = new Scanner(System.in);
+            GameManager gm = new GameManager(scanner, ConnectionManager.getSQLConnection());
+
+            //step 1 - display the menu (pickup a hero)
+            gm.displayMenu();
+
+            //step 2 - choose an area (all quests in the area will be assigned to the Hero)
+            gm.chooseArea();
+
+            //step 3 - battle (kill one monster for completing one quest)
+            gm.battle();
+
+            //step 4 - display the result (trigger: die or choose 'quit')
+            gm.displayReport();
+
+            //step 5 - restore all the data back to normal and close the Scanner
+            gm.restore();
+
+            //Close scanner and database connection
+            scanner.close();
+            ConnectionManager.closeConnection();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
