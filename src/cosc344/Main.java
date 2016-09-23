ee76c496d9e78eba27f730aa4244cbee6ead2c90
albertgao <albertgaohy@gmail.com>
2016-09-23 17:16:12 +1200

    optimize some description

--- src/cosc344/Main.java
+++ src/cosc344/Main.java
@@ -8,33 +8,34 @@ import java.sql.SQLException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        try {
-            Scanner scanner = new Scanner(System.in);
-            GameManager gm = new GameManager(scanner, ConnectionManager.getSQLConnection());
+	public static void main(String[] args) {
+		try {
+			Scanner scanner = new Scanner(System.in);
+			GameManager gm = new GameManager(scanner, ConnectionManager.getSQLConnection());
 
-            //step 1 - display the menu (pickup a hero)
-            gm.displayMenu();
+			// step 1 - display the menu (pickup a hero)
+			gm.displayMenu();
 
-            //step 2 - pick a weapon according to the hero you choose
-            gm.pickWeapon();
-            
-            //step 3 - choose an area (all quests in the area will be assigned to the Hero)
-            gm.chooseArea();
+			// step 2 - pick a weapon according to the hero you choose
+			gm.pickWeapon();
 
-            //step 4 - battle (kill one monster for completing one quest)
-            gm.battle();
+			// step 3 - choose an area (all quests in the area will be assigned
+			// to the Hero)
+			gm.chooseArea();
 
-            //step 5 - display the result (trigger: die or choose 'quit')
-            gm.displayReport();
+			// step 4 - battle (kill one monster for completing one quest)
+			gm.battle();
 
-            //step 7 - create a json for this report
+			// step 5 - display the result (trigger: die or choose 'quit')
+			gm.displayReport();
 
-            //Close scanner and database connection
-            scanner.close();
-            ConnectionManager.closeConnection();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+			// step 7 - create a json for this report
+
+			// Close scanner and database connection
+			scanner.close();
+			ConnectionManager.closeConnection();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
